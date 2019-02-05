73513dd5c75e99bfcda79c6bb4b61c43648d849b
tayyebi <tayyebimohammadreza@gmail.com>
2019-02-06 01:06:55 +0330

    New File and Folder

--- program/main-gui.py
+++ program/main-gui.py
@@ -1,10 +1,18 @@
 import os
+import os.path
 import pwd
 
 from PyQt5 import QtCore, QtGui, QtWidgets
 
+from PyQt5.QtWidgets import QTreeWidgetItem, QInputDialog, QLineEdit, QApplication
+from PyQt5.QtGui import QIcon
+
 import gui
 import main
+
+dir_path = os.path.split(os.path.abspath(os.path.dirname(os.path.realpath(__file__))))[0] + '/content'
+_translate = QtCore.QCoreApplication.translate
+current_dir = current_selected = dir_path
 
 def get_username():
     return pwd.getpwuid( os.getuid() )[ 0 ]
@@ -14,8 +22,6 @@ def helloworld():
     print ('Hello World')
 
 def load_project_structure(startpath, tree):
-    from PyQt5.QtWidgets import QTreeWidgetItem
-    from PyQt5.QtGui import QIcon
     for element in os.listdir(startpath):
         path_info = startpath + "/" + element
         parent_itm = QTreeWidgetItem(tree, [os.path.basename(element)])
@@ -31,19 +37,31 @@ def getItemFullPath(item):
     if item.parent():
         out = getItemFullPath(item.parent()) + "/" + out
     else:
-        out =  "../content/" + out
-    return out;
+        out =  dir_path + "/" + out
+    return out
 
 def newFolder():
-    print ('New Folder')
+    name = QInputDialog.getText(None,"New Folder","Enter the name")
+    print ('new folder: ' + current_dir + "/" + name[0] + ".md")
+    os.mkdir(current_dir + "/" + name[0])
+    load_project_structure(dir_path,ui.treeWidget)
 
 def newFile():
-    print ('New File')
-
+    name = QInputDialog.getText(None,"New File","Enter the name")
+    print('new file: ' + current_dir + "/" + name[0] + ".md")
+    open(current_dir + "/" + name[0] + ".md", 'a').close()
+    load_project_structure(dir_path,ui.treeWidget)
 
 def onItemClicked(it, col):
     # print(it, col, it.text(col))
-    print(getItemFullPath(it))
+    current_dir = current_selected = getItemFullPath(it)
+    if os.path.isfile(current_selected):
+        current_dir = os.path.dirname(current_selected)
+    print(current_dir)
+    ui.statusbar.showMessage(get_username() + ': ' + current_selected)
+    if os.path.isfile(current_selected):
+        with open(current_selected) as f:
+            ui.plainTextEdit.setPlainText(_translate("MainWindow", ''.join(f.readlines())))
 
 if __name__ == "__main__":
     import sys
@@ -51,12 +69,10 @@ if __name__ == "__main__":
     MainWindow = QtWidgets.QMainWindow()
     ui = gui.Ui_MainWindow()
     ui.setupUi(MainWindow)
-
-    dir_path = os.path.split(os.path.abspath(os.path.dirname(os.path.realpath(__file__))))[0]
+    
     load_project_structure(
-        dir_path + '/content'
+        dir_path
         ,ui.treeWidget)
-    ui.statusbar.showMessage(get_username() + ': ' + dir_path)
     ui.treeWidget.itemClicked.connect(onItemClicked)
     ui.actionBuild.triggered.connect(main.Run)
     ui.actionFile.triggered.connect(newFile)
