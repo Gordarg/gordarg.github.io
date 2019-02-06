7554a4ebaa08b92f92993d9329a0b9145fc6d94a
tayyebi <tayyebimohammadreza@gmail.com>
2019-02-06 13:57:40 +0330

    Delete function

--- program/main-gui.py
+++ program/main-gui.py
@@ -1,10 +1,11 @@
 import os
 import os.path
 import pwd
+import shutil
 
 from PyQt5 import QtCore, QtGui, QtWidgets
 
-from PyQt5.QtWidgets import QTreeWidgetItem, QInputDialog, QLineEdit, QApplication
+from PyQt5.QtWidgets import QTreeWidgetItem, QInputDialog, QLineEdit, QApplication, QMessageBox
 from PyQt5.QtGui import QIcon
 
 import gui
@@ -12,14 +13,16 @@ import main
 
 dir_path = os.path.split(os.path.abspath(os.path.dirname(os.path.realpath(__file__))))[0] + '/content'
 _translate = QtCore.QCoreApplication.translate
-current_dir = current_selected = dir_path
+current_selected = dir_path
+current_dir = ''
 
 def get_username():
     return pwd.getpwuid( os.getuid() )[ 0 ]
 
-
-def helloworld():
-    print ('Hello World')
+def reset_project_structure():
+    #TODO: Make it possible
+    #pass
+    print('Hello world')
 
 def load_project_structure(startpath, tree):
     for element in os.listdir(startpath):
@@ -44,16 +47,35 @@ def newFolder():
     name = QInputDialog.getText(None,"New Folder","Enter the name")
     print ('new folder: ' + current_dir + "/" + name[0] + ".md")
     os.mkdir(current_dir + "/" + name[0])
+    reset_project_structure()
     load_project_structure(dir_path,ui.treeWidget)
 
 def newFile():
     name = QInputDialog.getText(None,"New File","Enter the name")
     print('new file: ' + current_dir + "/" + name[0] + ".md")
     open(current_dir + "/" + name[0] + ".md", 'a').close()
+    reset_project_structure()
     load_project_structure(dir_path,ui.treeWidget)
 
-def onItemClicked(it, col):
-    # print(it, col, it.text(col))
+def delete():
+    reply = QMessageBox.question(None, 'Delete File', 
+        'Are you sure to delete following?\n' +
+        current_selected
+        , QMessageBox.Yes, QMessageBox.No)
+    
+    if reply == QMessageBox.Yes:
+        if os.path.isdir(current_selected):
+            shutil.rmtree(current_selected)
+        else:
+            os.remove(current_selected)
+        
+        reset_project_structure()
+        load_project_structure(dir_path,ui.treeWidget)
+
+def selectionChanged():
+    it = ui.treeWidget.selectedItems()[0]
+    global current_dir
+    global current_selected
     current_dir = current_selected = getItemFullPath(it)
     if os.path.isfile(current_selected):
         current_dir = os.path.dirname(current_selected)
@@ -62,6 +84,8 @@ def onItemClicked(it, col):
     if os.path.isfile(current_selected):
         with open(current_selected) as f:
             ui.plainTextEdit.setPlainText(_translate("MainWindow", ''.join(f.readlines())))
+    else:
+        ui.plainTextEdit.setPlainText(_translate("MainWindow", ''))
 
 if __name__ == "__main__":
     import sys
@@ -73,10 +97,12 @@ if __name__ == "__main__":
     load_project_structure(
         dir_path
         ,ui.treeWidget)
-    ui.treeWidget.itemClicked.connect(onItemClicked)
+    
+    ui.treeWidget.itemSelectionChanged.connect(selectionChanged)
     ui.actionBuild.triggered.connect(main.Run)
     ui.actionFile.triggered.connect(newFile)
     ui.actionFolder.triggered.connect(newFolder)
+    ui.actionDelete.triggered.connect(delete)
 
     MainWindow.show()
     sys.exit(app.exec_())
